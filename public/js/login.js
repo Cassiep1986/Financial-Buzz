4228b556c77db31931776dfd99821c4858e484dd
Miguel Lozano <Miguellozano37578@gmail.com>
2021-07-10 14:31:05 -0700

    database changes

--- public/js/login.js
+++ public/js/login.js
@@ -2,17 +2,17 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email-login').value.trim();
+  const userName = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
+  if (userName && password) {
     // Send a POST request to the API endpoint
     const response = await fetch('/api/users/login', {
       method: 'POST',
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ userName, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-
+    
     if (response.ok) {
       // If successful, redirect the browser to the profile page
       document.location.replace('/profile');
