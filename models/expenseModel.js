d47004adada49c4ef70c871251156507cfd1e06c
St.Onge <stonge.ms@gmail.com>
2021-07-11 14:12:46 -0700

    adding models

---
+++ models/expenseModel.js
@@ -0,0 +1,52 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Expense extends Model {}
+
+Expense.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    amount: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    due_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Expense',
+  }
+);
+
+module.exports = Expense;
--- seeds/seed.js
+++ seeds/seed.js
@@ -2,7 +2,8 @@ const sequelize = require('../config/connection');
 const { User, Project } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const incomeData = require('./incomeData.json');
+const expenseData = require('./expenseData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
