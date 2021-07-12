f85be44763d275f7b9028124a1b499a52421a392
seacrest <cassiep1986@gmail.com>
2021-07-12 11:21:16 -0700

    fixed income route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const expenseRoutes = require('./expenseRoutes');
-const expenseRoutes = require('./incomeRoutes');
+const incomeRoutes = require('./incomeRoutes');
 
 router.use('/users', userRoutes);
 router.use('/expenses', expenseRoutes);
-router.use('/income', expenseRoutes);
+router.use('/income', incomeRoutes);
 
 module.exports = router;
